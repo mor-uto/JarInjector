dbfe880a402262194c59f32c67382bc5dce031f1
Mor-uto <morutomc@gmail.com>
2024-09-23 20:22:59 +0300

    Improved the getMainMethod(JarLoader) function

--- src/main/java/me/moruto/JarInjector.java
+++ src/main/java/me/moruto/JarInjector.java
@@ -3,35 +3,45 @@ package me.moruto;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
-import java.util.List;
+import java.util.zip.ZipEntry;
 
 public class JarInjector implements Opcodes {
-    public static void inject(String injectionMainClass, List<ClassNode> classes) {
-        MethodNode mainMethodNode = getMainMethod(classes);
+    public static void inject(String injectionMainClass, JarLoader loader) {
+        MethodNode mainMethodNode = getMainMethod(loader);
         if (mainMethodNode == null) {
-            GUI.log("Main method not found in the specified class. ");
+            GUI.log("Main method not found in the specified class.");
             return;
         }
 
-        InsnList injectionInstructions = new InsnList();
-        injectionInstructions.add(new TypeInsnNode(NEW, injectionMainClass));
-        injectionInstructions.add(new InsnNode(DUP));
-        injectionInstructions.add(new MethodInsnNode(INVOKESPECIAL, injectionMainClass, "<init>", "()V", false));
+        InsnList insnList = new InsnList();
+        insnList.add(new TypeInsnNode(NEW, injectionMainClass));
+        insnList.add(new InsnNode(DUP));
+        insnList.add(new MethodInsnNode(INVOKESPECIAL, injectionMainClass, "<init>", "()V", false));
 
-        mainMethodNode.instructions.insertBefore(mainMethodNode.instructions.getFirst(), injectionInstructions);
+        mainMethodNode.instructions.insertBefore(mainMethodNode.instructions.getFirst(), insnList);
 
         GUI.log("Injection successful!");
     }
 
-    private static MethodNode getMainMethod(List<ClassNode> classes) {
-        for (ClassNode classNode : classes) {
-            for (MethodNode methodNode : classNode.methods) {
-                if (methodNode.name.equalsIgnoreCase("main") && methodNode.desc.equals("([Ljava/lang/String;)V")) {
-                    return methodNode;
+    private static MethodNode getMainMethod(JarLoader loader) {
+        ZipEntry manifest = loader.getManifest();
+        String mainClass = null;
+
+        if (manifest != null) {
+            mainClass = new String(manifest.getExtra()).split("Main-Class: ")[1].split("\n")[0].replace(".", "/");
+        }
+
+        for (ClassNode classNode : loader.getClasses()) {
+            if (mainClass == null || classNode.name.equals(mainClass)) {
+                for (MethodNode methodNode : classNode.methods) {
+                    if (methodNode.name.equalsIgnoreCase("main") && methodNode.desc.equals("([Ljava/lang/String;)V")) {
+                        return methodNode;
+                    }
                 }
             }
         }
 
         return null;
     }
+
 }
